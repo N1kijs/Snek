fffd41b2aae1b3e1d00bb210d94ffbca6effcc29
N1kijs <48949270+N1kijs@users.noreply.github.com>
2019-11-12 09:57:46 +0200

    Update led.py

--- led.py
+++ led.py
@@ -2,14 +2,55 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.setup(4,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
 
-button = 4
-
-GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
-while True:
-    button_state = GPIO.input(button)
-    if button_state == GPIO.HIGH:
-        print ("HIGH")
-    else:
-        print ("LOW")
-    time.sleep(0.5)
+GPIO.setup(22, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, GPIO.PUD_UP)
+try:
+    while True:
+        button_state1 = GPIO.input(22)
+        button_state2 = GPIO.input(23)
+        button_state3 = GPIO.input(24)
+        if button_state1 == GPIO.LOW:
+            GPIO.output(4,GPIO.HIGH)
+        if button_state2 == GPIO.LOW:
+            GPIO.output(17,GPIO.HIGH)
+        if button_state3 == GPIO.LOW:
+            GPIO.output(27,GPIO.HIGH)
+        if button_state1 == GPIO.LOW and button_state2 == GPIO.LOW and button_state3 == GPIO.LOW:
+            GPIO.output(4,GPIO.LOW)
+            GPIO.output(17,GPIO.LOW)
+            GPIO.output(27,GPIO.LOW)
+            GPIO.output(4,GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(4,GPIO.LOW)
+            time.sleep(1)
+            GPIO.output(17,GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(17,GPIO.LOW)
+            time.sleep(1)
+            GPIO.output(27,GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(27,GPIO.LOW)
+            GPIO.output(4,GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(4,GPIO.LOW)
+            time.sleep(1)
+            GPIO.output(17,GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(17,GPIO.LOW)
+            time.sleep(1)
+            GPIO.output(27,GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(27,GPIO.LOW)
+        else:
+            GPIO.output(4,GPIO.LOW)
+            GPIO.output(17,GPIO.LOW)
+            GPIO.output(27,GPIO.LOW)
+except KeyboardInterrupt:
+   GPIO.cleanup()
+finally:
+    GPIO.cleanup()
