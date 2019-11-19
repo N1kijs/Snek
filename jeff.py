bb0ffcdbbb7797b942b11159ba8d4e9e162ee407
N1kijs <48949270+N1kijs@users.noreply.github.com>
2019-11-19 09:40:54 +0200

    Update jeff.py

--- jeff.py
+++ jeff.py
@@ -1,6 +1,18 @@
 import smbus2
 import bme280
 
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(4,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+
+GPIO.setup(22, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, GPIO.PUD_UP)
+
 port = 1
 address = 0x76
 bus = smbus2.SMBus(port)
@@ -9,10 +21,26 @@ claibration_params = bme280.load_calibration_params(bus, address)
 
 data = bme280.sample(bus, address, claibration_params)
 
-print(data.id)
-print(data.timestamp)
-print(data.temperature)
-print(data.pressure)
-print(data.humidity)
+try:
+    while True:
+        button_state1 = GPIO.input(22)
+        button_state2 = GPIO.input(23)
+        button_state3 = GPIO.input(24)
+        if button_state1 == GPIO.LOW:
+            GPIO.output(4,GPIO.HIGH)
+            print(data.timestamp)
+        if button_state2 == GPIO.LOW:
+            GPIO.output(17,GPIO.HIGH)
+            print(data.pressure)
+        if button_state3 == GPIO.LOW:
+            GPIO.output(27,GPIO.HIGH)
+            print(data.temperature)
+        else:
+            GPIO.output(4,GPIO.LOW)
+            GPIO.output(17,GPIO.LOW)
+            GPIO.output(27,GPIO.LOW)
+except KeyboardInterrupt:
+   GPIO.cleanup()
+finally:
+    GPIO.cleanup()
 
-print(data)
