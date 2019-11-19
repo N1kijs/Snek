cf7d58aa325b819ee7682bff336f5b0891376602
N1kijs <48949270+N1kijs@users.noreply.github.com>
2019-11-19 09:08:31 +0200

    Update jeff.py

--- jeff.py
+++ jeff.py
@@ -5,7 +5,7 @@ port = 1
 address = 0x76
 bus = smbus2.SMBus(port)
 
-claibration_params = bme280.lead_calibration_params(bus, address)
+claibration_params = bme280.load_calibration_params(bus, address)
 
 data = bme280.sample(bus, address, claibration_params)
 
